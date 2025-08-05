3073e1e501dcfdcce625c53caa9fec8997953ed6
SaraVSSILVA <saravitoriads@outlook.com>
2025-08-05 12:56:38 -0300

    feat: cria a lógica Componente Deletar Postagenns

--- src/components/temas/deletarpostagem/DeletarPostagem.tsx
+++ src/components/temas/deletarpostagem/DeletarPostagem.tsx
@@ -1,4 +1,79 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useContext, useEffect } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { AuthContext } from "../../../contexts/AuthContext"
+import type Post from "../../../models/Post"
+import { buscar, deletar } from "../../../services/Service"
+import { RotatingLines } from "react-loader-spinner"
+
 function DeletarPostagem() {
+
+    const navigate = useNavigate()
+
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [postagem, setPostagem] = useState<Post>({} as Post)
+
+    const { id } = useParams<{ id: string }>()
+
+    const { user, handleLogout } = useContext(AuthContext)
+    const token = user.token
+
+    async function buscarPorId(id: string) {
+        try {
+            await buscar(`/posts/${id}`, setPostagem, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
+    }
+
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado')
+            navigate('/')
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id)
+        }
+    }, [id])
+
+    async function deletarPostagem() {
+        setIsLoading(true)
+
+        try {
+            await deletar(`/posts/${id}`, {
+                headers: {
+                    'Authorization': token
+                }
+            })
+
+            alert('Postagem apagada com sucesso')
+
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }else {
+                alert('Erro ao deletar a postagem.')
+            }
+        }
+
+        setIsLoading(false)
+        retornar()
+    }
+
+    function retornar() {
+        navigate("/postagens")
+    }
+    
     return (
         <div className='container w-1/3 mx-auto'>
             <h1 className='text-4xl text-center my-4'>Deletar Postagem</h1>
@@ -13,18 +88,30 @@ function DeletarPostagem() {
                     Postagem
                 </header>
                 <div className="p-4">
-                    <p className='text-xl h-full'>Título da Postagem</p>
-                    <p>Texto da Postagem</p>
+                    <p className='text-xl h-full'>{postagem.title}</p>
+                    <p>{postagem.text}</p>
                 </div>
                 <div className="flex">
                     <button 
-                        className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2'>
+                        className='text-slate-100 bg-red-400 hover:bg-red-600 w-full py-2'
+                        onClick={retornar}>
                         Não
                     </button>
                     <button 
                         className='w-full text-slate-100 bg-indigo-400 
-                        hover:bg-indigo-600 flex items-center justify-center'>
-                        Sim
+                        hover:bg-indigo-600 flex items-center justify-center'
+                        onClick={deletarPostagem}>
+                        
+                        {isLoading ?
+                            <RotatingLines
+                                strokeColor="white"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                width="24"
+                                visible={true}
+                            /> :
+                            <span>Sim</span>
+                        }
                     </button>
                 </div>
             </div>
